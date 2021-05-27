32a0554feaf72ccfa3ecf47c025d6fda87ebbee8
carsonteh <carson.teh1205@gmail.com>
2021-05-27 15:44:11 +1000

    made some changes

--- factlead.sql
+++ factlead.sql
@@ -5,12 +5,12 @@ from
 select count(LEadId) lead
 from OAWarehouse..Fact_Lead
 where DateLeadCreated >= '2021-05-01'
-and MarketSubChannelId = 5
+and MarketSubChannelId = 4
 )L
 left join 
 (
 select count(LEadId) intros
 from OAWarehouse..Fact_Lead
 where DateLeadHotIntro >= '2021-05-01'
-and MarketSubChannelId = 5
-)
+and MarketSubChannelId = 4
+)L
